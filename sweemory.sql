dc4e86c92ac1a01da4bf9009ebeb25da9b418eb5
c-ncl <nicolecanlapanb@gmail.com>
2023-04-16 21:51:02 -0400

    Update sweemory.sql

--- sweemory.sql
+++ sweemory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2023 at 03:33 AM
+-- Generation Time: Apr 17, 2023 at 03:46 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -26,27 +26,181 @@ USE `sweemory`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `admin_id` int(6) NOT NULL,
+  `user_id` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE `employee` (
+  `emp_id` int(6) NOT NULL,
+  `user_id` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ingredient`
+--
+
+DROP TABLE IF EXISTS `ingredient`;
+CREATE TABLE `ingredient` (
+  `ingredient_id` int(6) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `arrival_date` date NOT NULL,
+  `quantity` int(3) NOT NULL,
+  `expired_date` date NOT NULL,
+  `description` varchar(220) NOT NULL,
+  `price` float NOT NULL,
+  `picture` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `itspecialist`
+--
+
+DROP TABLE IF EXISTS `itspecialist`;
+CREATE TABLE `itspecialist` (
+  `it_id` int(6) NOT NULL,
+  `user_id` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `itspecialist`
+--
+
+INSERT INTO `itspecialist` (`it_id`, `user_id`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message` (
+  `message_id` int(6) NOT NULL,
+  `receiver` varchar(50) NOT NULL,
+  `sender` varchar(50) NOT NULL,
+  `message` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+DROP TABLE IF EXISTS `notification`;
+CREATE TABLE `notification` (
+  `notify_id` int(6) NOT NULL,
+  `notify_type` varchar(24) NOT NULL,
+  `message` varchar(125) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `product_id` int(6) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `produced_date` date NOT NULL,
+  `quantity` int(3) NOT NULL,
+  `expired_date` date NOT NULL,
+  `description` varchar(220) NOT NULL,
+  `price` float NOT NULL,
+  `picture` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
+  `user_id` int(6) NOT NULL,
+  `username` varchar(30) NOT NULL,
   `password_hash` varchar(72) NOT NULL,
-  `user_type` varchar(12) NOT NULL
+  `user_type` varchar(12) NOT NULL,
+  `status` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `username`, `password_hash`, `user_type`) VALUES
-(1, 'itspecialist', '$2y$10$pfRMoKFjKDZUlPK0zmaD1O77qsFpu32IliJGRYQE75e1trgptkYuy', 'itspecialist');
+INSERT INTO `user` (`user_id`, `username`, `password_hash`, `user_type`, `status`) VALUES
+(1, 'itspecialist', '$2y$10$pfRMoKFjKDZUlPK0zmaD1O77qsFpu32IliJGRYQE75e1trgptkYuy', 'itspecialist', 'active');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD KEY `user_to_admin` (`user_id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`emp_id`),
+  ADD KEY `user_to_employee` (`user_id`);
+
+--
+-- Indexes for table `ingredient`
+--
+ALTER TABLE `ingredient`
+  ADD PRIMARY KEY (`ingredient_id`,`name`,`arrival_date`);
+
+--
+-- Indexes for table `itspecialist`
+--
+ALTER TABLE `itspecialist`
+  ADD PRIMARY KEY (`it_id`),
+  ADD KEY `user_to_itspecialist` (`user_id`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`message_id`);
+
+--
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`notify_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`,`name`,`produced_date`);
 
 --
 -- Indexes for table `user`
@@ -59,10 +213,74 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `emp_id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ingredient`
+--
+ALTER TABLE `ingredient`
+  MODIFY `ingredient_id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `itspecialist`
+--
+ALTER TABLE `itspecialist`
+  MODIFY `it_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `message_id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `notify_id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `admin`
+--
+ALTER TABLE `admin`
+  ADD CONSTRAINT `user_to_admin` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `user_to_employee` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `itspecialist`
+--
+ALTER TABLE `itspecialist`
+  ADD CONSTRAINT `user_to_itspecialist` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
