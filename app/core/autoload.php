8ba450977162365487ddc1080470645f40ff60eb
c-ncl <nicolecanlapanb@gmail.com>
2023-04-18 13:19:58 -0400

    Making our code compatible with Linux, UNIX and macOS.

--- app/core/autoload.php
+++ app/core/autoload.php
@@ -2,6 +2,7 @@
 spl_autoload_register(
     function($class_name) 
     {
-        require_once($class_name . '.php');
+        $class_name = str_replace("\\", DIRECTORY_SEPARATOR, $class_name);
+		require_once($class_name . '.php');
     }
 );
