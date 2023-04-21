946b2fa6ebb928cf4bf5083f62f693e0a056a0ae
c-ncl <89605841+c-ncl@users.noreply.github.com>
2023-04-21 14:22:20 -0400

    still experimenting with the navbar. i got two sidebars on each side to be working.

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -1,4 +1,5 @@
 <?php $this->view('shared/header','Home Feed'); ?>
+<br><br><br><br><br><br>
 <a class="btn btn-primary" href="/User/logout">Log Out</a>
 <?php $this->view('shared/navigation/nav'); ?>
 
