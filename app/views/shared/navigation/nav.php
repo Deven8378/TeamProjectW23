78d9a524070cfcb1fbe84a47b22f944e26318dbe
c-ncl <nicolecanlapanb@gmail.com>
2023-04-21 02:23:29 -0400

    working and experimenting on the navbar.

--- app/views/shared/navigation/nav.php
+++ app/views/shared/navigation/nav.php
@@ -1,12 +1,84 @@
 <nav class="navbar bg-body-tertiary fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/Main/index">Sweemory</a>
-
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+    <a class="navbar-brand" href="/Main/index">Sweemory</a>
+    
+
+
+
+
+
+
+    <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Notification (<b>2</b>)</a>
+          <ul class="dropdown-menu notify-drop">
+            <div class="notify-drop-title">
+              <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-6">Bildirimler (<b>2</b>)</div>
+                <div class="col-md-6 col-sm-6 col-xs-6 text-right"><a href="" class="rIcon allRead" data-tooltip="tooltip" data-placement="bottom" title="tümü okundu."><i class="fa fa-dot-circle-o"></i></a></div>
+              </div>
+            </div>
+            <!-- end notify title -->
+            <!-- notify content -->
+            <div class="drop-content">
+              <li>
+                <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""></div></div>
+                <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+                
+                <hr>
+                <p class="time">Şimdi</p>
+                </div>
+              </li>
+              <li>
+                <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""></div></div>
+                <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+                <p>Lorem ipsum sit dolor amet consilium.</p>
+                <p class="time">1 Saat önce</p>
+                </div>
+              </li>
+              <li>
+                <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""></div></div>
+                <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+                <p>Lorem ipsum sit dolor amet consilium.</p>
+                <p class="time">29 Dakika önce</p>
+                </div>
+              </li>
+              <li>
+                <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""></div></div>
+                <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+                <p>Lorem ipsum sit dolor amet consilium.</p>
+                <p class="time">Dün 13:18</p>
+                </div>
+              </li>
+              <li>
+                <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""></div></div>
+                <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+                <p>Lorem ipsum sit dolor amet consilium.</p>
+                <p class="time">2 Hafta önce</p>
+                </div>
+              </li>
+            </div>
+            <div class="notify-drop-footer text-center">
+              <a href=""><i class="fa fa-eye"></i> Tümünü Göster</a>
+            </div>
+          </ul>
+        </li>
+      </ul>
+
+
+
+
+
+
+
+
+    
+
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
 
       <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Notifications</h5>
@@ -16,27 +88,7 @@
       <div class="offcanvas-body">
 
         <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-body-tertiary" style="width: 380px;">
-
-          <div class="list-group list-group-flush border-bottom scrollarea">
-
-            <a href="#" class="list-group-item list-group-item-action active py-3 lh-sm" aria-current="true">
-              <div class="d-flex w-100 align-items-center justify-content-between">
-                <strong class="mb-1">About to Expire: Banana Ice Cream</strong>
-                <small>Wed</small>
-              </div>
-              <div class="col-10 mb-1 small">Banana Ice Cream is about to expire on April 30th, 2023. To see more, click here.</div>
-            </a>
-
-            <a href="#" class="list-group-item list-group-item-action py-3 lh-sm">
-              <div class="d-flex w-100 align-items-center justify-content-between">
-                <strong class="mb-1">Low on Stock: Bananas</strong>
-                <small class="text-body-secondary">Tues</small>
-              </div>
-              <div class="col-10 mb-1 small">Banana Ice Cream is low on stock. You have 7 more. To see more, click here.</div>
-            </a>
-
-          </div>
-
+          <?php $this->view('shared/notification_partial'); ?>
         </div>
 
         <form class="d-flex mt-3" role="search">
@@ -45,6 +97,5 @@
         </form>
 
       </div>
-    </div>
   </div>
 </nav>
---
+++ app/views/shared/notification_partial.php
@@ -0,0 +1,19 @@
+<div class="list-group list-group-flush border-bottom scrollarea">
+
+  <a href="#" class="list-group-item list-group-item-action active py-3 lh-sm" aria-current="true">
+    <div class="d-flex w-100 align-items-center justify-content-between">
+      <strong class="mb-1">About to Expire: Banana Ice Cream</strong>
+      <small>Wed</small>
+    </div>
+    <div class="col-10 mb-1 small">Banana Ice Cream is about to expire on April 30th, 2023. To see more, click here.</div>
+  </a>
+  
+  <a href="#" class="list-group-item list-group-item-action py-3 lh-sm">
+    <div class="d-flex w-100 align-items-center justify-content-between">
+      <strong class="mb-1">Low on Stock: Bananas</strong>
+      <small class="text-body-secondary">Tues</small>
+    </div>
+    <div class="col-10 mb-1 small">Banana Ice Cream is low on stock. You have 7 more. To see more, click here.</div>
+  </a>
+
+</div>
