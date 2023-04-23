e9174895be43726b935a2bdbda6d82fec9b5d4f8
Mubeenkh <mubkhan@gmail.com>
2023-04-23 18:44:26 -0400

    did some css for create user page

--- app/views/ITspecialist/createUser.php
+++ app/views/ITspecialist/createUser.php
@@ -1,16 +1,16 @@
 <?php $this->view('shared/header','Create User'); ?>
 <?php $this->view('shared/errorAndSuccessMessages'); ?>
 
-<div class="main">
+<!-- <div class="createUser" style="border:1px solid black;">
 	
 	<p class="sign" align="center"><?=_('Create a new user')?></p>
 	<form method="post" action="" class="form1">
-		<input class="username" type="text" align="center" placeholder="<?=_('Username')?>" name="username">
-		<input class="password" type="password" align="center" placeholder="<?=_('Password')?>" name="password">
+		<input class="createUsername" type="text" align="center" placeholder="<?=_('Username')?>" name="username">
+		<input class="createUsername" type="password" align="center" placeholder="<?=_('Password')?>" name="password">
 		<br>
 
 		<div align="center">
-			<select name="user_type" id="user_type" >
+			<select name="user_type" id="user_type"  class="form-select " style="">
 				<option selected disabled>--<?=_('Select a User Type')?>--</option>
 				<option value="admin" name="admin">Admin</option>
 				<option value="employee" name="employee">Employee</option>
@@ -21,9 +21,35 @@
 		<input type="submit" id="submitLink" name="action" class="submit" align="center" value="<?= _('Create User')?>">
 		<a href="/ITspecialist/index">Back</a>
 	</form>
-</div>
+</div> -->
 
+<!-- <div class="container" style=""> -->
+	<div class="createUser" align="center">
 
+		<p class="sign" align="center"><?=_('Create a new user')?></p>
+		<form method="post" action="" class="form1" style="" >
+
+			<!-- <label>Username:</label> -->
+			<input class="createInput" type="text" align="" placeholder="<?=_('Username')?>" name="username">
+
+			<!-- <label>Password:</label> -->
+			<input class="createInput" type="password" align="center" placeholder="<?=_('Password')?>" name="password">
+
+			<!-- <div align="center" style="align-items: center;"> -->
+				<select name="user_type" id="user_type"  class="dropdownUserType" style="">
+					<option selected disabled>--<?=_('Select a User Type')?>--</option>
+					<option value="admin" name="admin">Admin</option>
+					<option value="employee" name="employee">Employee</option>
+				</select>
+			<!-- </div> -->
+			
+
+			<input type="submit" id="submitLink" name="action" class="submitUser" align="" value="<?= _('Create User')?>">
+			<br>
+			<a href="/ITspecialist/index">Back</a>
+		</form>
+	</div>
+<!-- </div> -->
 
 
 <?php $this->view('shared/footer'); ?>
--- app/views/ITspecialist/index.php
+++ app/views/ITspecialist/index.php
@@ -1,54 +1,59 @@
 <?php $this->view('shared/header','IT Home Page'); ?>
 <?php $this->view('shared/navigation/nav'); ?>
 
-<div class="p-4 mt-5 border d-flex">
+<div class="container" style="padding-top: 50px;">
+	<div class="p-4 mt-5 border d-flex">
 
-	<div class="border">
-		<a href="/ITspecialist/createUser"><?=_('Add User')?></a>
-		
-	</div>
-	<div class="spacer" style="width: 8px; flex-shrink: 0; margin: 0; padding: 0;"></div>
+		<div class="border">
+			<a href="/ITspecialist/createUser"><?=_('Add User')?></a>
+			
+		</div>
+		<div class="spacer" style="width: 8px; flex-shrink: 0; margin: 0; padding: 0;"></div>
 
-	<div class="border">
-		
-		<h2><?=_('List of Employee and Admin')?></h2>
+		<div class="" style="">
+			
+			<h2><?=_('List of Employee and Admin')?></h2>
 
-		<table class="table table-striped table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">Status</th>
-                    <th scope="col">ID</th>
-                    <th scope="col">Username</th>
-                    <th scope="col">First Name</th>
-                    <th scope="col">Middle Name</th>
-                    <th scope="col">Last Name</th>
-                    <th scope="col">email</th>
-                    <th scope="col">Phone Number</th>
+			<div class="" style="border: 1px solid grey; border-radius: 10px;">
 
-                </tr>
-            </thead>
-            <tbody>
-            	<?php foreach ($data as $user) { ?>
-	                <tr>
-	                    <th scope="row"><?=$user->status?></th>
-	                    <td><a href="/ITspecialist/viewUserDetails/<?=$user->user_id?>"><?=$user->user_id?></a></td>
-	                    <td><?=$user->username?></td>
-	                    <td><?=$user->first_name?></td>
-	                    <td><?=$user->middle_name?></td>
-	                    <td><?=$user->last_name?></td>
-	                    <td><?=$user->email?></td>
-	                    <td><?=$user->phone_number?></td>
-	                </tr>
-                <?php
-					}
-				?>
-            
-            </tbody>
-        </table>
+				<table class="table table-striped " style="">
+					<!-- table-striped table-bordered -->
+		            <thead class="thead-dark">
+		                <tr >
+		                    <th scope="col">Status</th>
+		                    <th scope="col">ID</th>
+		                    <th scope="col">Username</th>
+		                    <th scope="col">First Name</th>
+		                    <th scope="col">Middle Name</th>
+		                    <th scope="col">Last Name</th>
+		                    <th scope="col">email</th>
+		                    <th scope="col">Phone Number</th>
 
-	</div>
-
-
+		                </tr>
+		            </thead>
+		            <tbody class="">
+		            	<?php foreach ($data as $user) { ?>
+			                <tr>
+			                    <th scope="row"><?=$user->status?></th>
+			                    <td><a href="/ITspecialist/viewUserDetails/<?=$user->user_id?>"><?=$user->user_id?></a></td>
+			                    <td><?=$user->username?></td>
+			                    <td><?=$user->first_name?></td>
+			                    <td><?=$user->middle_name?></td>
+			                    <td><?=$user->last_name?></td>
+			                    <td><?=$user->email?></td>
+			                    <td><?=$user->phone_number?></td>
+			                </tr>
+		                <?php
+							}
+						?>
+		            
+		            </tbody>
+		        </table>
+		    </div>
+		</div>
 	
+	</div>
+
 </div>
 <?php $this->view('shared/footer'); ?>
+
