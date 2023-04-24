58bb3b35cabfa33580ba53311f439db5270b8a14
Mubeenkh <mubkhan@gmail.com>
2023-04-23 20:00:52 -0400

    did some css for viewUserDetails

--- app/views/ITspecialist/createUser.php
+++ app/views/ITspecialist/createUser.php
@@ -1,27 +1,6 @@
 <?php $this->view('shared/header','Create User'); ?>
+<?php $this->view('shared/navigation/nav'); ?>
 <?php $this->view('shared/errorAndSuccessMessages'); ?>
-
-<!-- <div class="createUser" style="border:1px solid black;">
-	
-	<p class="sign" align="center"><?=_('Create a new user')?></p>
-	<form method="post" action="" class="form1">
-		<input class="createUsername" type="text" align="center" placeholder="<?=_('Username')?>" name="username">
-		<input class="createUsername" type="password" align="center" placeholder="<?=_('Password')?>" name="password">
-		<br>
-
-		<div align="center">
-			<select name="user_type" id="user_type"  class="form-select " style="">
-				<option selected disabled>--<?=_('Select a User Type')?>--</option>
-				<option value="admin" name="admin">Admin</option>
-				<option value="employee" name="employee">Employee</option>
-			</select>
-		</div>
-		<br>
-
-		<input type="submit" id="submitLink" name="action" class="submit" align="center" value="<?= _('Create User')?>">
-		<a href="/ITspecialist/index">Back</a>
-	</form>
-</div> -->
 
 <!-- <div class="container" style=""> -->
 	<div class="createUser" align="center">
