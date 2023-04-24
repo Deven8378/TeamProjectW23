db4fe82aee7c9de0e84413fb865026962b561f03
Mubeenkh <89649704+Mubeenkh@users.noreply.github.com>
2023-04-24 18:00:47 -0400

    idk what changes i made but i havent pushed in a while

--- app/views/ITspecialist/index.php
+++ app/views/ITspecialist/index.php
@@ -2,18 +2,26 @@
 <?php $this->view('shared/navigation/itnav'); ?>
 
 <div class="container" style="padding-top: 20px;">
-	<div class="p-4 mt-5 border d-flex">
+
+	<div class="viewUsers">
+		<div>
+
+			<input class="input-radio" type="radio" name="">
+			<label>Admins</label>
+
+		</div>
+
 		<div class="spacer" style="width: 8px; flex-shrink: 0; margin: 0; padding: 0;"></div>
 
 		<div class="" style="">
 			
 			<h2><?=_('List of Employee and Admin')?></h2>
 
-			<div class="" style="border: 1px solid grey; border-radius: 10px;">
+			<!-- <div class="" style="border: 1px solid grey; border-radius: 10px;"> -->
 
-				<table class="table table-striped " style="">
+				<table class="table " style="">
 					<!-- table-striped table-bordered -->
-		            <thead class="thead-dark">
+		            <thead class="table-secondary">
 		                <tr >
 		                    <th scope="col">Status</th>
 		                    <th scope="col">ID</th>
@@ -31,7 +39,7 @@
 			                <tr>
 			                    <th scope="row"><?=$user->status?></th>
 			                    <td><a href="/ITspecialist/viewUserDetails/<?=$user->user_id?>"><?=$user->user_id?></a></td>
-			                    <td><?=$user->username?></td>
+			                    <td style=""><?=$user->username?></td>
 			                    <td><?=$user->first_name?></td>
 			                    <td><?=$user->middle_name?></td>
 			                    <td><?=$user->last_name?></td>
@@ -44,7 +52,7 @@
 		            
 		            </tbody>
 		        </table>
-		    </div>
+		    <!-- </div> -->
 		</div>
 	
 	</div>
