3e4b9a1858c10b77e2b2a2c8a94aa99e9ec5a32c
Mubeenkh <89649704+Mubeenkh@users.noreply.github.com>
2023-04-25 20:07:45 -0400

    (Nicole) more implementing i18n and htmlentitites

--- app/views/Recipe/edit.php
+++ app/views/Recipe/edit.php
@@ -5,16 +5,16 @@
 	<p class="sign" align="center"><?=_('Edit this Recipe')?></p>
 	<form action='' method='post' enctype="multipart/form-data">
 
-		<input class="createInput" name="title" type="text" align="" placeholder="<?=_('Title')?>" value= <?="'$data->title'"?> >
+		<input class="createInput" name="title" type="text" align="" placeholder="<?=_('Title')?>" value="<?= htmlentities($data->title) ?>">
 
-		<textarea class="createInput"name="description" placeholder="<?=_('Description')?>" rows="10" cols="50"> <?= "$data->description" ?> </textarea>
+		<textarea class="createInput"name="description" placeholder="<?=_('Description')?>" rows="10" cols="50"><?= htmlentities($data->description) ?></textarea>
 
 
-		<input class="createInput" type="file" align="" placeholder="<?=_('Picture')?>" name="recipePicture" value = <?= "$data->picture" ?> >
+		<input class="createInput" type="file" align="" placeholder="<?=_('Picture')?>" name="recipePicture" value ="<?= htmlentities($data->picture) ?>" >
 
 		<input class="submitUser" type="submit" align="" placeholder="<?=_('Add Recipe')?>" name="action"> <br>
 
-		<?= "<a href='/Recipe/details/$data->recipe_id'>" ?>  <?= _("Back") ?> </a> 
+		<a href='/Recipe/details/<?= htmlentities($data->recipe_id) ?>'><?= _("Back") ?></a> 
 	 
 	</form>
 
