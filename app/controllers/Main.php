f3fbc4b2f684788ed86faaf938f527018f47eea9
Mubeenkh <89649704+Mubeenkh@users.noreply.github.com>
2023-04-29 15:46:23 -0400

    inactive users cant login anymore

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 class Main extends \app\core\Controller
 {
     #[\app\filters\ProfileCreated]
+    #[\app\filters\Status]
     #[\app\filters\Login]
     #[\app\filters\EmployeeAndAdmin]
     public function index()
