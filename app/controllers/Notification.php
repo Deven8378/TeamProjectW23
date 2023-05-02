59988e5c90340ac90bcb334ae4ab3a824a7fbb99
c-ncl <89605841+c-ncl@users.noreply.github.com>
2023-05-02 11:49:07 -0400

    Update Notification.php

--- app/controllers/Notification.php
+++ app/controllers/Notification.php
@@ -13,41 +13,41 @@ class Notification extends \app\core\Controller
 
 	public function create()
 	{
-		if(isset($_POST['action'])){
-			$treshold = new \app\models\Treshold();
-			$ingredients = new \app\models\Ingredient();
-			$ingredients = $ingredients->getIngredientandQuantity();
-			$notification = new \app\models\Notification();
+		// if(isset($_POST['action'])){
+		// 	$treshold = new \app\models\Treshold();
+		// 	$ingredients = new \app\models\Ingredient();
+		// 	$ingredients = $ingredients->getIngredientandQuantity();
+		// 	$notification = new \app\models\Notification();
 
-			foreach ($ingredients as $ingredient) {
-				$expiry_date = $ingredient->expiry_date;
-				$treshold = $treshold->getSpecificTreshold($ingredient->category);
-				if($ingredient->daysLeft <= $treshold 
-					&& $ingredient->daysLeft > 0){
-					$notification->notify_type = "expiring";
-					$notification->insert();
-				} else if($ingredient->quantity <= 10) {
-					$notification->notify_type = "lowStock";
-					$notification->insert();
-				}
-			}
+		// 	foreach ($ingredients as $ingredient) {
+		// 		$expiry_date = $ingredient->expiry_date;
+		// 		$treshold = $treshold->getSpecificTreshold($ingredient->category);
+		// 		if($ingredient->daysLeft <= $treshold 
+		// 			&& $ingredient->daysLeft > 0){
+		// 			$notification->notify_type = "expiring";
+		// 			$notification->insert();
+		// 		} else if($ingredient->quantity <= 10) {
+		// 			$notification->notify_type = "lowStock";
+		// 			$notification->insert();
+		// 		}
+		// 	}
 
-			$products = new \app\models\Product();
-			$products = $products->getProductandQuantity();
-			foreach ($products as $product) {
-				$expiry_date = $product->expiry_date;
-				$treshold = $treshold->getSpecificTreshold($product->category);
+		// 	$products = new \app\models\Product();
+		// 	$products = $products->getProductandQuantity();
+		// 	foreach ($products as $product) {
+		// 		$expiry_date = $product->expiry_date;
+		// 		$treshold = $treshold->getSpecificTreshold($product->category);
 
-				if($product->daysLeft <= $treshold 
-					&& $product->daysLeft > 0){
-					$notification->notify_type = "expiring";
-					$notification->insert();
-				} else if($product->quantity <= 10) {
-					$notification->notify_type = "lowStock";
-					$notification->insert();
-				}
-			}
-		}
+		// 		if($product->daysLeft <= $treshold 
+		// 			&& $product->daysLeft > 0){
+		// 			$notification->notify_type = "expiring";
+		// 			$notification->insert();
+		// 		} else if($product->quantity <= 10) {
+		// 			$notification->notify_type = "lowStock";
+		// 			$notification->insert();
+		// 		}
+		// 	}
+		// }
 	}
 
 	#[\app\filters\EmployeeAndAdmin]
