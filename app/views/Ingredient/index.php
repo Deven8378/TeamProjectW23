452ba8816f9ed1b3c1e9b2226600b4ed8abeeb1d
c-ncl <89605841+c-ncl@users.noreply.github.com>
2023-05-04 20:49:32 -0400

    pushingggg

--- app/views/Ingredient/index.php
+++ app/views/Ingredient/index.php
@@ -3,7 +3,7 @@
 <?php $this->view('shared/navigation/switchToIngredients'); ?>
 <style type="text/css">
 	.vl {
-	  border-left: 1px solid #d9d9d9;
+	  border-right: 1px solid #d9d9d9;
 	  height: 100%;
 	}
 	.rowing{
@@ -17,12 +17,15 @@
 		<div class="col-sm-2" style="padding-right: 10px;">
 			<div class="rowing" style="width: 200px; display: flex;  width: 100%; justify-content: center;">
 				<div>
-					<a class="btn" href="/Ingredient/createIngredient" role="button" style="background-color: #e8c8e7;">
+					<a class="btn" href="/Ingredient/createIngredient" role="button" style="background-color: #e8c8e7; width: 150px;">
 						<?= _('Add Ingredient') ?>
 					</a>
 				</div>
+			</div>
+			<br>
+			<div class="rowing" style="width: 200px; display: flex;  width: 100%; justify-content: center;">
 				<div>
-					<a href="/Category/index"  class="btn" style="background-color:#e8c8e8;">
+					<a href="/Category/index"  class="btn" style="background-color:#e8c8e8; width: 150px;">
 						<?= _('View categories') ?>
 					</a>
 				</div>
@@ -30,41 +33,58 @@
 
 			<hr style="height:1px; border-width:0 ;color: #d9d9d9; background-color:gray">
 
-			<div class="btn-group">
-			  <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
-			    <?= _('Categories') ?>
-			  </button>
-			  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
-			  	<?php 
-			  		foreach ($data[1] as $category) { ?>
-			  			<li>
-					    	<button class="dropdown-item" type="button">
-					    		<?= $category->category_name ?>
-					    	</button>
-					    </li>
-			  		<?php }
-			  	?>
-			  </ul>
+
+			<div class="rowing" style="width: 200px; display: flex;  width: 100%; justify-content: center;">
+				<div>
+					<div class="btn-group">
+					  <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+					    <?= _('Categories') ?>
+					  </button>
+					  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
+					  	<?php 
+					  		foreach ($data[1] as $category) { ?>
+					  			<li>
+							    	<button class="dropdown-item" type="button">
+							    		<?= $category->category_name ?>
+							    	</button>
+							    </li>
+					  		<?php }
+					  	?>
+					  </ul>
+					</div>
+				</div>
 			</div>
-			<div class="btn-group">
-			  <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
-			    <?= _('Availabilities') ?>
-			  </button>
-			  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
-			    <li><button class="dropdown-item" type="button"><?= _('Available') ?></button></li>
-			    <li><button class="dropdown-item" type="button"><?= _('Low on Stock') ?></button></li>
-			    <li><button class="dropdown-item" type="button"><?= _('About to Expire') ?></button></li>
-			    <li><button class="dropdown-item" type="button"><?= _('Expired') ?></button></li>
-			  </ul>
+			<div class="rowing" style="width: 200px; display: flex;  width: 100%; justify-content: center;">
+				<div>
+					<div class="btn-group">
+					  <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+					    <?= _('Availabilities') ?>
+					  </button>
+					  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
+					    <li><button class="dropdown-item" type="button"><?= _('Available') ?></button></li>
+					    <li><button class="dropdown-item" type="button"><?= _('Low on Stock') ?></button></li>
+					    <li><button class="dropdown-item" type="button"><?= _('About to Expire') ?></button></li>
+					    <li><button class="dropdown-item" type="button"><?= _('Expired') ?></button></li>
+					  </ul>
+					</div>
+				</div>
 			</div>
+			
 		</div>
 		<div class="col-sm-1"> 
 			<div class="vl"></div>
 		</div>
 		<div class="col offset-sm-1">
-			<div class="row">
-			  <?php $this->view('Ingredient/ingredientsCard', $data[0]); ?>
+			<div class="rowing" style="width: 200px; display: flex;  width: 100%; justify-content: center;">
+				<div>
+					<div class="row">
+					  <?php $this->view('Ingredient/ingredientsCard', $data[0]); ?>
+					</div>
+				</div>
 			</div>
+
+
+
 		</div>
 	</div>
 </div>
