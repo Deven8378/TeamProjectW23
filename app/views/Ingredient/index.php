0e06e2e408cc03dd65652a7b2a3705792cc03aa0
Mubeenkh <89649704+Mubeenkh@users.noreply.github.com>
2023-05-08 00:00:38 -0400

    i hate this page

--- app/views/Ingredient/index.php
+++ app/views/Ingredient/index.php
@@ -8,66 +8,67 @@
 
 <div class="container">
 	<div class="row-index">
-		<div class="col-index">
-			<?php if($data[3] == true) { ?>
-					<div class="centering">
-						<div>
-							<a class="btn-general" href="/Ingredient/createIngredient" role="button" style="width: 150px;">
-								<?= _('Add Ingredient') ?>
-							</a>
+		<div>
+			<div class="col-index" style="">
+				<?php if($data[3] == true) { ?>
+						<!-- <div class="centering"> -->
+							<div class="grid-btn-one">
+								<a class="btn-general" href="/Ingredient/createIngredient" role="button" style="width: 150px;">
+									<?= _('Add Ingredient') ?>
+								</a>
+							</div>
+						<!-- </div> -->
+
+						<!-- <div class="centering"> -->
+							<div class="grid-btn-two">
+								<a href="/Category/index" class="btn-general" style="width: 150px;">
+									<?= _('View categories') ?>
+								</a>
+							</div>
+						<!-- </div> -->
+
+						<hr class="col-dividor">
+				<?php } ?>
+
+				<!-- <div class="centering"> -->
+					<a href="/Ingredient/index" class="grid-btn-three">
+						<button style="width: 150px;" class="btn-general" type="button">
+			  				<?= _('Reset') ?>
+			  			</button>
+			  		</a>
+				<!-- </div> -->
+				<!-- <div class="centering"> -->
+					<!-- <div> -->
+						<div class="btn-group grid-btn-four" style="display: grid;" >
+
+						  <button style="width: 150px;" type="button" class="btn dropdown-toggle btn-general" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+						  	<?= _('Categories') ?>
+						  		
+						  	</button>
+
+						  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
+
+						  	<?php 
+						  		foreach ($data[1] as $category) { ?>
+						  			<li>
+						  				<form action="/Ingredient/filterByCategory/<?= $category->category_id ?>" method="post">
+											<input class="dropdown-item" style=" width: 100%;" type="submit" name="" value="<?= $category->category_name ?>">
+										</form>
+								    </li>
+						  		<?php }
+						  	?>
+						  </ul>
 						</div>
-					</div>
-					<br>
-					<div class="centering">
-						<div>
-							<a href="/Category/index" class="btn-general" style="width: 150px;">
-								<?= _('View categories') ?>
-							</a>
-						</div>
-					</div>
-
-					<hr class="col-dividor">
-			<?php } ?>
-
-			<div class="centering">
-				<a href="/Ingredient/index">
-					<button style="width: 150px;" class="btn-general" type="button">
-		  				<?= _('Reset') ?>
-		  			</button>
-		  		</a>
-			</div>
-			<div class="centering">
-				<!-- <div> -->
-					<!-- <div class="btn-group"> -->
-					  <button style="width: 150px;" type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
-					    <?= _('Categories') ?>
-					  </button>
-
-					  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
-
-					  	<?php 
-					  		foreach ($data[1] as $category) { ?>
-					  			<li>
-					  				<form action="/Ingredient/filterByCategory/<?= $category->category_id ?>" method="post">
-										<input class="dropdown-item" style=" width: 100%;" type="submit" name="" value="<?= $category->category_name ?>">
-									</form>
-							    </li>
-					  		<?php }
-					  	?>
-					  </ul>
 					<!-- </div> -->
 				<!-- </div> -->
+
 			</div>
-
 		</div>
-
-		<div class="row-dividor"> 
-			<div class="vl"></div>
-		</div>
-
+		<div class="row-dividor"><div class="vl"></div></div>
+<!-- ------------------------------------------------ -->
 		<div class="col-content">
 			<!-- <div class="rowing" style=""> -->
-				<div style="display: flex; justify-content: space-between; margin-bottom: 20px; margin-right: 10px;">
+				<div style="display: flex; justify-content: space-between; margin-bottom: 20px; margin-right: 50px;">
 
 					<p style="font-weight: bold; font-size: 50"><?= $data[2]->num_results ?> <?= _('Results') ?></p>
 
