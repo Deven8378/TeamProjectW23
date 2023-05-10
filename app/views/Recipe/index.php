a93506f7984588ff34ea04ab5731048a4fb3cd55
c-ncl <nicolecanlapanb@gmail.com>
2023-05-10 03:49:01 -0400

    fixing stuff

--- app/views/Recipe/index.php
+++ app/views/Recipe/index.php
@@ -4,13 +4,11 @@
 <div class="container">
 	<div class="sign recipe-sign" align="center"><?= _('Recipes Page') ?></div>
 	<div class="row-recipe">
-		<div class="col-recipe">
-			<a class="btn-general recipe-btn" href="/Recipe/create" role="button"><?= _('Add Recipe') ?></a>
-			<hr class="btn-recipe-divider">	
-		</div>
-		<div class="col-recipe-divider"> 
-			<div class="recipe-divider"></div>
-		</div>
+		<?php if($_SESSION['user_type'] == "admin"){ ?>
+			<div class="col-recipe">
+				<a class="btn-general recipe-btn" href="/Recipe/create" role="button"><?= _('Add Recipe') ?></a>
+			</div>
+		<?php } ?>
 		<div class="card-ml">
 			<div class="card-content" align="center">
 			  <?php $this->view('Recipe/recipeCard', $data); ?>
